b7d0adcc65e50b0a712e0c3a0c9cc0eb7e39b105
Reshan <reshanmadushanka@gmail.com>
2022-02-15 07:04:12 +0530

    update login successs event create

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Listeners\LoginSuccessful;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,7 +19,11 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        'Illuminate\Auth\Events\Login' => [
+            LoginSuccessful::class,
+        ],
     ];
+
 
     /**
      * Register any events for your application.
